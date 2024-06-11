4c153c3b1453c5b0a04733ae9cfce441311a20e2
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-06-11 02:06:08 +0200

    updates

--- multichat/settings.py
+++ multichat/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-y5%o!nltc075ls)r9+3^cgkm4c5pa(8&m(@8pl+-mgjr+bjsqt
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://multi-chat.io', 'https://api.multi-chat.io', 'http://localhost:3000', 'http://localhost:8000']
+ALLOWED_HOSTS = ['api.multi-chat.io', 'http://localhost:3000', 'http://localhost:8000']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
