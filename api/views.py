6fe273d3e5f309fec2cd16a31c76a9607f365d20
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-04 17:57:26 +0200

    updates

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from main.models import User, Chat, MessagingService
+# from main.models import User, Chat, MessagingService
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from main.models import User, MessagingService, Chat
+# from main.models import User, MessagingService, Chat
 
 
 class AccountAdmin(UserAdmin):
