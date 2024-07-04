db0d74fcd6d2a0ac16757484ca556419d4356bda
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-04 17:55:00 +0200

    updates

--- main/models.py
+++ main/models.py
@@ -24,31 +24,31 @@ class BaseModel(models.Model):
 #         db_table = "subscription"
 
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, name=None):
-        if not email:
-            raise ValueError("Users must have an email address.")
-        if not username:
-            raise ValueError("Users must have a username.")
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            name=name
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# class MyUserManager(BaseUserManager):
+#     def create_user(self, email, username, password=None, name=None):
+#         if not email:
+#             raise ValueError("Users must have an email address.")
+#         if not username:
+#             raise ValueError("Users must have a username.")
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             username=username,
+#             name=name
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
 
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
-        )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+#     def create_superuser(self, email, username, password):
+#         user = self.create_user(
+#             email=self.normalize_email(email),
+#             username=username,
+#             password=password,
+#         )
+#         user.is_staff = True
+#         user.is_superuser = True
+#         user.save(using=self._db)
+#         return user
 
 
 # class User(AbstractBaseUser):
