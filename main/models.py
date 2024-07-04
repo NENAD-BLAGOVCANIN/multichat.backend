20e9984e767eeb2491c304fd8c34b009730e457c
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-04 18:42:29 +0200

    updates

--- main/models.py
+++ main/models.py
@@ -63,8 +63,8 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
-    notifications = models.BooleanField(default=True)
-    audio_notifications = models.BooleanField(default=True)
+    # notifications = models.BooleanField(default=True)
+    # audio_notifications = models.BooleanField(default=True)
     # subscription = models.ForeignKey(
     #     Subscription, on_delete=models.SET_NULL, null=True
     # )
