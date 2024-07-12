53acc9d10493860ad7030ca1d9e84f73067f7734
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-13 01:25:40 +0200

    updates

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import createNewChat, editChat, getChats, deleteChat, registerUser, getMyUserInfo
+from .views import createNewChat, editChat, getChats, deleteChat, registerUser, getMyUserInfo, paymentReceived
 
 urlpatterns = [
 
