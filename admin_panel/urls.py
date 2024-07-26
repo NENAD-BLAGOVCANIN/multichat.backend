74b657318fd72656b742973c1225d85e3715b4c8
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-07-26 15:59:16 +0200

    updates

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -4,6 +4,6 @@ from .views import get_dashboard_stats, get_monthly_earnings
 urlpatterns = [
 
     path('dashboard-stats/', get_dashboard_stats, name='admin.get_dashboard_stats'),
-    path('dashboard-stats/monthly-earnings', get_monthly_earnings, name='admin.get_monthly_earnings'),
+    path('earnings', get_monthly_earnings, name='admin.get_monthly_earnings'),
 
 ]
